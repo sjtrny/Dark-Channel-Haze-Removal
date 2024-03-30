7515f99a2f86e0ee27ff708c6a6f5cb5cd6d3f43
Stephen Tierney <sjtrny@gmail.com>
2024-03-30 14:02:26 +1100

    Correct get_atmoshperic
    Based on https://github.com/sjtrny/Dark-Channel-Haze-Removal/issues/4

--- demo.m
+++ demo.m
@@ -3,7 +3,7 @@ warning('off','all');
 tic;
 image = double(imread('forest.jpg'))/255;
 
-image = imresize(image, 0.1);
+image = imresize(image, 0.5);
 
 result = dehaze(image, 0.95, 15);
 toc;
--- get_atmosphere.m
+++ get_atmosphere.m
@@ -3,20 +3,25 @@ function atmosphere = get_atmosphere(image, dark_channel)
 [m, n, ~] = size(image);
 n_pixels = m * n;
 
-n_search_pixels = floor(n_pixels * 0.01);
+n_search_pixels = floor(n_pixels * 0.001);
 
+% Reshape sources
 dark_vec = reshape(dark_channel, n_pixels, 1);
-
 image_vec = reshape(image, n_pixels, 3);
 
+% Calculate intensities of brightest pixels in dark channel
+% Warning: this is an approximation since averaging RGB may not correspond
+% to perceptual intensity. Consider alternatives.
 [~, indices] = sort(dark_vec, 'descend');
+atmosphere_search_pixels = image_vec(indices(1:n_search_pixels),:);
+intensities = mean(atmosphere_search_pixels,2);
 
-accumulator = zeros(1, 3);
+% Find the greatest intensity pixels
+[~, sorted_intensity_indices] = sort(intensities, 'descend');
+max_intensity = intensities(sorted_intensity_indices(1),:);
+top_intensity_indices = intensities == max_intensity;
 
-for k = 1 : n_search_pixels
-    accumulator = accumulator + image_vec(indices(k),:);
-end
-
-atmosphere = accumulator / n_search_pixels;
+% Get the mean RGB of brightest dark channel pixels
+atmosphere = mean(atmosphere_search_pixels(top_intensity_indices,:),1);
 
 end
